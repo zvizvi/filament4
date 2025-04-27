102d2313e923b486db84cbd4b96793ac910410c9
zvizvi <5633910@gmail.com>
2025-04-27 04:52:35 +0300

    Add isReadOnly method to LessonsRelationManager

--- app/Filament/Admin/Resources/Courses/RelationManagers/LessonsRelationManager.php
+++ app/Filament/Admin/Resources/Courses/RelationManagers/LessonsRelationManager.php
@@ -13,6 +13,11 @@ class LessonsRelationManager extends RelationManager
 
     protected static ?string $relatedResource = LessonResource::class;
 
+    public function isReadOnly(): bool
+    {
+        return false;
+    }
+
     public function table(Table $table): Table
     {
         return $table
