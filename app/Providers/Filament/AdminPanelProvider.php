c2a178c2f56886e353a0427aa8b829b03cb5d801
zvizvi <5633910@gmail.com>
2025-05-04 01:35:38 +0300

    Add filament-tweaks package with styles and scripts

--- app/Providers/Filament/AdminPanelProvider.php
+++ app/Providers/Filament/AdminPanelProvider.php
@@ -57,8 +57,6 @@ class AdminPanelProvider extends PanelProvider
             ])
             ->authMiddleware([
                 Authenticate::class,
-            ])
-            ->readOnlyRelationManagersOnResourceViewPagesByDefault(false)
-            ->spa();
+            ]);
     }
 }
